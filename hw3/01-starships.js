4bd256c2c0c50b13720758ba972f6be4a1359a17
Caterina Paun <caterina.paun@gmail.com>
2025-02-18 16:36:55 -0800

    Add: Starter files for hw 3

---
+++ hw3/01-starships.js
@@ -0,0 +1,12 @@
+// Note: Your JavaScript code should be inside of functions
+
+const fetchData = (url) => {
+  // Retrieve the data from the API
+};
+
+// Use this function to create the individual spaceship component
+const createSpaceshipComponent = (spaceship) => {
+  const container = document.createElement('section');
+
+  return container;
+};
